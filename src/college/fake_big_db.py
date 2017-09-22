81501b4215e320d2ecee85561ce34bf26e655e25
Roman Yasinovskyy <yasiro01@luther.edu>
2017-09-22 09:05:47 -0500

    Large DB generator done

--- src/college/fake_big_db.py
+++ src/college/fake_big_db.py
@@ -1,7 +1,6 @@
 '''Create a fake university database'''
 
 #!/usr/bin/env python3
-#encoding: UTF-8
 
 import random
 import sqlite3
@@ -9,20 +8,36 @@ from faker import Faker
 
 random.seed(440)
 
+STUDENT_TBL_SIZE = 25000
+DEPT_TBL_SIZE = 50  # Number of majors in the majors.txt
+COURSE_TBL_SIZE = 500
+SECTION_TBL_SIZE = 2000
+ENROLL_TBL_SIZE = 500000
+FACULTY_COUNT = 1000
+
 DEPT_LST = []
-FAC_LST = [Faker().name() for _ in range(100)]
+FAC_LST = [Faker().name() for _ in range(FACULTY_COUNT)]
 GRADES = ['A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'F']
-TITLES_ADV = ["Introduction to", "Mastering", "Developing", "Advanced", "Hacking", "Understanding", "Diving into"]
-TITLES_ADJ = ["Java", "C++", "Python", "Secure", "Computer", "Hardware", "Database", "Web", "Game", "Operating"]
-TITLES_NOUN = ["Basics", "Fundamentals", "Concepts", "Systems", "Security", "Problems", "Networks", "Principles"]
+TITLES_ADV = ['Advanced', 'Building', 'Designing', 'Developing',
+              'Diving into', 'Elementary', 'Hacking', 'In search of',
+              'Introduction to', 'Mastering', 'Teach Yourself', 'Understanding']
+TITLES_ADJ = ['Algorithms', 'C++', 'Computer', 'Database',
+              'Game', 'Java', 'Hardware', 'Network',
+              'Python', 'Secure', 'Operating', 'Web']
+TITLES_NOUN = ['Basics', 'Concepts', 'Design', 'for Dummies',
+               'Fundamentals', 'in 10 Days', 'in a Nutshell', 'Networks',
+               'Principles', 'Problems', 'Security', 'Systems']
 
 
-def title():
+def get_course_title():
     '''Generate the course title'''
     if random.random() > 0.5:
-        return random.choice(TITLES_ADV) + ' ' + random.choice(TITLES_ADJ) + ' ' + random.choice(TITLES_NOUN)
+        return random.choice(TITLES_ADV) + ' '\
+               + random.choice(TITLES_ADJ) + ' '\
+               + random.choice(TITLES_NOUN)
     else:
         return random.choice(TITLES_ADJ) + ' ' + random.choice(TITLES_NOUN)
+
 
 def read_files():
     '''Read data from files'''
@@ -30,91 +45,99 @@ def read_files():
         for line in input_file:
             DEPT_LST.append(line.strip())
 
+
 def create_db(conn):
     '''Create a database'''
     cursor = conn.cursor()
     # Create table STUDENT
-    cursor.execute("DROP TABLE IF EXISTS student;")
+    cursor.execute('DROP TABLE IF EXISTS student;')
     cursor.execute('''CREATE TABLE student (
                             SId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                            SName TEXT,
-                            GradYear INTEGER,
-                            MajorId INTEGER);''')
+                            SName TEXT NOT NULL,
+                            GradYear INTEGER NOT NULL,
+                            MajorId INTEGER NOT NULL,
+                            FOREIGN KEY (MajorId) REFERENCES dept(DId)
+                    );''')
     # Create table DEPT
-    cursor.execute("DROP TABLE IF EXISTS dept;")
+    cursor.execute('DROP TABLE IF EXISTS dept;')
     cursor.execute('''CREATE TABLE dept (
                             DId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                            DName TEXT);''')
+                            DName TEXT NOT NULL
+                    );''')
     # Create table COURSE
-    cursor.execute("DROP TABLE IF EXISTS course;")
+    cursor.execute('DROP TABLE IF EXISTS course;')
     cursor.execute('''CREATE TABLE course (
                             CId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                            Title TEXT,
-                            DeptId INTEGER);''')
+                            Title TEXT NOT NULL,
+                            DeptId INTEGER NOT NULL,
+                            FOREIGN KEY (DeptId) REFERENCES dept(DId)
+                    );''')
     # Create table SECTION
-    cursor.execute("DROP TABLE IF EXISTS section;")
+    cursor.execute('DROP TABLE IF EXISTS section;')
     cursor.execute('''CREATE TABLE section (
                             SectId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                            CourseId INTEGER,
-                            Prof TEXT,
-                            YearOffered INTEGER);''')
+                            CourseId INTEGER NOT NULL,
+                            Prof TEXT NOT NULL,
+                            YearOffered INTEGER NOT NULL,
+                            FOREIGN KEY (CourseId) REFERENCES course(CId)
+                    );''')
     # Create table ENROLL
-    cursor.execute("DROP TABLE IF EXISTS enroll;")
+    cursor.execute('DROP TABLE IF EXISTS enroll;')
     cursor.execute('''CREATE TABLE enroll (
                             EId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                            StudentId INTEGER,
-                            SectionId INTEGER,
-                            Grade TEXT);''')
+                            StudentId INTEGER NOT NULL,
+                            SectionId INTEGER NOT NULL,
+                            Grade TEXT NOT NULL,
+                            FOREIGN KEY (StudentId) REFERENCES student(SId),
+                            FOREIGN KEY (SectionId) REFERENCES section(SectId)
+                    );''')
     conn.commit()
-                        
+
+
 def populate_db(conn):
     '''Add date to the database'''
     cursor = conn.cursor()
     person = Faker()
-    number_of_students = 25000
-    number_of_departments = len(DEPT_LST)
-    number_of_courses = 500
-    number_of_sections = 2000
-    number_of_enrollments = 50000
-
-    # Populate table STUDENT
-    for _ in range(25000):
-        cursor.execute("INSERT INTO student(SName, GradYear, MajorId) VALUES ('{}', {}, {});".\
-            format(person.name(), random.randint(1861, 2017), random.randint(1, number_of_departments)))
-    conn.commit()
+    cursor.execute("PRAGMA foreign_keys = ON;")
     # Populate table DEPT
     for dept in DEPT_LST:
-        cursor.execute("INSERT INTO dept(DName) VALUES ('{}');".\
-            format(dept))
+        cursor.execute("INSERT INTO dept(DName) VALUES ('{}');".
+                       format(dept))
+    conn.commit()
+    # Populate table STUDENT
+    for _ in range(STUDENT_TBL_SIZE):
+        cursor.execute("INSERT INTO student(SName, GradYear, MajorId) VALUES ('{}', {}, {});".
+                       format(person.name(), random.randint(1861, 2017), random.randint(1, DEPT_TBL_SIZE)))
     conn.commit()
     # Populate table COURSE
     # Course can be offered once by a department
     courses = set()
-    while len(courses) < number_of_courses:
-        course_title = title()
-        dept_id = random.randint(1, number_of_departments)
+    while len(courses) < COURSE_TBL_SIZE:
+        course_title = get_course_title()
+        dept_id = random.randint(1, DEPT_TBL_SIZE)
         if (course_title, dept_id) not in courses:
-            cursor.execute("INSERT INTO course(Title, DeptId) VALUES ('{}', {});".\
-                format(course_title, dept_id))
+            cursor.execute("INSERT INTO course(Title, DeptId) VALUES ('{}', {});".
+                           format(course_title, dept_id))
         courses.add((course_title, dept_id))
     conn.commit()
     # Populate table SECTION
-    for _ in range(number_of_sections):
-        cursor.execute("INSERT INTO section(CourseId, Prof, YearOffered) VALUES ({}, '{}', {});".\
-            format(random.randint(1, number_of_courses), random.choice(FAC_LST), random.randint(1861, 2017)))
+    for _ in range(SECTION_TBL_SIZE):
+        cursor.execute("INSERT INTO section(CourseId, Prof, YearOffered) VALUES ({}, '{}', {});".
+                       format(random.randint(1, COURSE_TBL_SIZE), random.choice(FAC_LST), random.randint(1861, 2017)))
     conn.commit()
     # Populate table ENROLL
     # A student can take a class once
     enrollments = set()
-    while len(enrollments) < number_of_enrollments:
-        student_id = random.randint(1, number_of_students)
-        section_id = random.randint(1, number_of_sections)
+    while len(enrollments) < ENROLL_TBL_SIZE:
+        student_id = random.randint(1, STUDENT_TBL_SIZE)
+        section_id = random.randint(1, SECTION_TBL_SIZE)
         if (student_id, section_id) not in enrollments:
-            cursor.execute("INSERT INTO enroll(StudentId, SectionId, Grade) VALUES ({}, {}, '{}');".\
-                format(student_id, section_id, random.choice(GRADES)))
+            cursor.execute("INSERT INTO enroll(StudentId, SectionId, Grade) VALUES ({}, {}, '{}');".
+                           format(student_id, section_id, random.choice(GRADES)))
         enrollments.add((student_id, section_id))
 
     conn.commit()
+
 
 def main():
     '''Main function'''
@@ -124,6 +147,5 @@ def main():
     populate_db(conn)
     conn.close()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
